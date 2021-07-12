2f3c4a44781f332d161a0d6ecf92be274420e9ce
King UIT <viethoa2410py>
2021-07-12 23:12:31 +0700

    first commit

---
+++ QuanLyThuVien/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace QuanLyThuVien.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region command
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MouseMoveWindowCommand { get; set; }
+        
+        #endregion
+
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.Close();
+                }
+            }
+            );
+            MouseMoveWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.DragMove();
+                }
+            }
+           );
+        }
+        FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+
+            while (parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+
+            return parent;
+        }
+    }
+}
---
+++ QuanLyThuVien/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace QuanLyThuVien.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded = false;
+        public ICommand LoadWindowCommand { get; set; }
+        public MainViewModel()
+        {
+            LoadWindowCommand = new RelayCommand<Object>((p) => { return true; }, (p) =>
+            {
+                Isloaded = true;
+                MessageBox.Show("Cre: 19520552");
+                Logscreen logscreen = new Logscreen();
+                logscreen.ShowDialog();
+            }
+            );
+        }
+    }
+}
