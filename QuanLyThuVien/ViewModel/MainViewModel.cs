fc0004c315e7ec88b1369771772e94a0ee6ff03f
King UIT <viethoa2410py>
2021-07-13 12:04:11 +0700

    Update UX/UI, add new screen

--- QuanLyThuVien/ViewModel/MainViewModel.cs
+++ QuanLyThuVien/ViewModel/MainViewModel.cs
@@ -12,6 +12,12 @@ namespace QuanLyThuVien.ViewModel
     {
         public bool Isloaded = false;
         public ICommand LoadWindowCommand { get; set; }
+        public ICommand PassbookCommand { get; set; }
+        public ICommand PaymentCommand { get; set; }
+        public ICommand ReceiptsCommand { get; set; }
+        public ICommand PassbookListViewCommand { get; set; }
+        public ICommand StatisticalCommand { get; set; }
+        public ICommand MonthStatisticalCommand { get; set; }
         public MainViewModel()
         {
             LoadWindowCommand = new RelayCommand<Object>((p) => { return true; }, (p) =>
@@ -22,6 +28,12 @@ namespace QuanLyThuVien.ViewModel
                 logscreen.ShowDialog();
             }
             );
+            PassbookCommand = new RelayCommand<Object>((p) => { return true; }, (p) => { Passbook wd = new Passbook(); wd.ShowDialog(); });
+            PaymentCommand = new RelayCommand<Object>((p) => { return true; }, (p) => { Payment wd = new Payment(); wd.ShowDialog(); });
+            ReceiptsCommand = new RelayCommand<Object>((p) => { return true; }, (p) => { Receipts wd = new Receipts(); wd.ShowDialog(); });
+            PassbookListViewCommand = new RelayCommand<Object>((p) => { return true; }, (p) => { PassbookListView wd = new PassbookListView(); wd.ShowDialog(); });
+            StatisticalCommand = new RelayCommand<Object>((p) => { return true; }, (p) => { Statistical wd = new Statistical(); wd.ShowDialog(); });
+            MonthStatisticalCommand = new RelayCommand<Object>((p) => { return true; }, (p) => { MonthStatistical wd = new MonthStatistical(); wd.ShowDialog(); });
         }
     }
 }
